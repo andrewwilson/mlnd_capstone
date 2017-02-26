abc7157fc9a3b825efa0c4075df83d0e5a557e2f
Andy Wilson <andywilson55@yahoo.com>
2017-02-26 13:10:44 +0000

    modified:   runNotebook.sh

--- runNotebook.sh
+++ runNotebook.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
+# 
+# Launch script to run jupyter notebook and record the remote access url in a text file.
+# useful for running the notebook on a remote workstation with dropbox syncing of the filesystem.
+#
+
 #enable job control
 set -m
 jupyter notebook --ip="*" 2> /tmp/url.txt &
