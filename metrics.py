4b3180d932bda9170ef6bb27c0202a313fe1f7c7
Andy Wilson <andywilson161@gmail.com>
2017-04-22 22:54:22 +0100

    more report write-up. code doco.

--- metrics.py
+++ metrics.py
@@ -11,6 +11,19 @@ def performance_report(name, price_series, lookahead, true_class, prediction,
                        histogram=False,
                        heatmap=False
                        ):
+    """
+    Generates a performance report
+    :param name: name of the model to report
+    :param price_series: the raw price series
+    :param lookahead: the number of periods look-ahead to consider for the future-return prediction
+    :param true_class: the true future return direction
+    :param prediction: the real valued prediction of the model
+    :param f1_detail: flag - should more details of f1 score be reported 
+    :param cum_return_plot: flag - should cumulative future return chart be drawn
+    :param histogram: flag - should a histogram of the predictions be drawn
+    :param heatmap: should a heatmap of actual versus predicted directions be drawn.
+    
+    """
     predicted_class = utils.prediction_to_category2(prediction)
 
     fut_returns = utils.future_return(price_series, lookahead).fillna(0)
@@ -48,6 +61,7 @@ def annualized_mean_future_return(pred_future_return, lookahead_minutes):
     num_periods_per_year = one_minute_periods_per_year/lookahead_minutes
     return ((pred_future_return.mean() + 1) ** (num_periods_per_year-1) ) -1
 
+
 def predicted_future_return(fut_returns, predicted_class, demean=True):
 
     if demean:
@@ -83,8 +97,6 @@ def aggregated_predicted_future_return(prices, prediction, agg_period, mode='sma
     mult[agg_predicted_class == 0] = -1
     mult[agg_predicted_class == 1] = 1
     return fut_returns * mult
-
-
 
 
 def fut_return_plot(name, pred_fut_return):
